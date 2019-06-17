b542c61a617383602c8e7d1e4bf428d5e8c69617
Ward Nica <ward.nica@kuleuven.be>
2019-06-17 19:21:41 +0200

    connects to station created by laptop, receives SocketMessages from ESP

--- main/tcp_server.cpp
+++ main/tcp_server.cpp
@@ -10,6 +10,7 @@
 #include <sys/param.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/event_groups.h"
 #include "esp_system.h"
 #include "esp_wifi.h"
 #include "esp_event.h"
@@ -17,6 +18,9 @@
 #include "nvs_flash.h"
 #include "tcpip_adapter.h"
 #include "protocol_examples_common.h"
+
+
+#include <tcpip_adapter.h>
 
 #include "lwip/err.h"
 #include "lwip/sockets.h"
@@ -29,11 +33,34 @@ extern "C" {
    void app_main(void);
 }
 
-#define PORT CONFIG_EXAMPLE_PORT
+#define PORT CONFIG_EXAMPLE_PORT //TCP Socket port defined in 'make menuconfig'.
+#define ENABLE_STATIC_IP
+/* The examples use WiFi configuration that you can set via 'make menuconfig'.
+   If you'd rather not, just change the below entries to strings with
+   the config you want - ie #define EXAMPLE_WIFI_SSID "mywifissid"
+*/
 
-static const char *TAG = "example";
+#define ESP1_WiFi_PSK	"0UtQ}fM0T]RjRC{}" //MAC:	30:ae:a4:05:d7:b8
+#define ESP2_WiFi_PSK	"jw3hgaX}>mrTEJLO" //MAC:	30:ae:a4:05:ef:5c
+#define ESP3_WiFi_PSK	"wh{nOcr>Z8DJNUrN" //MAC:	30:ae:a4:05:ac:38
+
+#define STATIC_IP		"10.128.164.33"//"10.42.0.33"
+#define SUBNET_MASK		"255.255.255.0"
+#define GATE_WAY		"10.128.164.254"
+#define DNS_SERVER		"8.8.8.8"
+
+#define ESP_WIFI_SSID    "PSK_GENT_LiTL"// "laptopWard"
+#define ESP_WIFI_PASS    ESP3_WiFi_PSK // "labo1234"
+#define ESP_WifiStation_MAXIMUM_RETRY  10
 
 
+
+tcpip_adapter_ip_info_t ipInfo;
+char str[256];
+
+
+#define TCP_ServerSocket_TAG "TCP Socket Server"
+//************************** TCP Socket Server
 static void tcp_server_task(void *pvParameters)
 {
     char rx_buffer[128];
@@ -51,7 +78,6 @@ static void tcp_server_task(void *pvParameters)
         addr_family = AF_INET;
         ip_protocol = IPPROTO_IP;
         inet_ntoa_r(dest_addr.sin_addr, addr_str, sizeof(addr_str) - 1);
-
 #else // IPV6
         struct sockaddr_in6 dest_addr;
         bzero(&dest_addr.sin6_addr.un, sizeof(dest_addr.sin6_addr.un));
@@ -64,45 +90,44 @@ static void tcp_server_task(void *pvParameters)
 
         int listen_sock = socket(addr_family, SOCK_STREAM, ip_protocol);
         if (listen_sock < 0) {
-            ESP_LOGE(TAG, "Unable to create socket: errno %d", errno);
+            ESP_LOGE(TCP_ServerSocket_TAG, "Unable to create socket: errno %d", errno);
             break;
         }
-        ESP_LOGI(TAG, "Socket created");
+        ESP_LOGI(TCP_ServerSocket_TAG, "Socket created");
 
         int err = bind(listen_sock, (struct sockaddr *)&dest_addr, sizeof(dest_addr));
         if (err != 0) {
-            ESP_LOGE(TAG, "Socket unable to bind: errno %d", errno);
+            ESP_LOGE(TCP_ServerSocket_TAG, "Socket unable to bind: errno %d", errno);
             break;
         }
-        ESP_LOGI(TAG, "Socket bound, port %d", PORT);
+        ESP_LOGI(TCP_ServerSocket_TAG, "Socket bound, port %d", PORT);
 
         err = listen(listen_sock, 1);
         if (err != 0) {
-            ESP_LOGE(TAG, "Error occurred during listen: errno %d", errno);
+            ESP_LOGE(TCP_ServerSocket_TAG, "Error occurred during listen: errno %d", errno);
             break;
         }
-        ESP_LOGI(TAG, "Socket listening");
+        ESP_LOGI(TCP_ServerSocket_TAG, "Socket listening");
 
         struct sockaddr_in6 source_addr; // Large enough for both IPv4 or IPv6
         uint addr_len = sizeof(source_addr);
         int sock = accept(listen_sock, (struct sockaddr *)&source_addr, &addr_len);
         if (sock < 0) {
-            ESP_LOGE(TAG, "Unable to accept connection: errno %d", errno);
+            ESP_LOGE(TCP_ServerSocket_TAG, "Unable to accept connection: errno %d", errno);
             break;
         }
-        ESP_LOGI(TAG, "Socket accepted");
-
+        ESP_LOGI(TCP_ServerSocket_TAG, "Socket accepted");
 
         while (1) {
             int len = recv(sock, rx_buffer, sizeof(rx_buffer) - 1, 0);
             // Error occurred during receiving
             if (len < 0) {
-                ESP_LOGE(TAG, "recv failed: errno %d", errno);
+                ESP_LOGE(TCP_ServerSocket_TAG, "recv failed: errno %d", errno);
                 break;
             }
             // Connection closed
             else if (len == 0) {
-                ESP_LOGI(TAG, "Connection closed");
+                ESP_LOGI(TCP_ServerSocket_TAG, "Connection closed");
                 break;
             }
             // Data received
@@ -115,19 +140,19 @@ static void tcp_server_task(void *pvParameters)
                 }
 
                 rx_buffer[len] = 0; // Null-terminate whatever we received and treat like a string
-                ESP_LOGI(TAG, "Received %d bytes from %s:", len, addr_str);
-                ESP_LOGI(TAG, "%s", rx_buffer);
+                ESP_LOGI(TCP_ServerSocket_TAG, "Received %d bytes from %s:", len, addr_str);
+                ESP_LOGI(TCP_ServerSocket_TAG, "%s", rx_buffer);
 
                 int err = send(sock, rx_buffer, len, 0);
                 if (err < 0) {
-                    ESP_LOGE(TAG, "Error occurred during sending: errno %d", errno);
+                    ESP_LOGE(TCP_ServerSocket_TAG, "Error occurred during sending: errno %d", errno);
                     break;
                 }
             }
         }
 
         if (sock != -1) {
-            ESP_LOGE(TAG, "Shutting down socket and restarting...");
+            ESP_LOGE(TCP_ServerSocket_TAG, "Shutting down socket and restarting...");
             shutdown(sock, 0);
             close(sock);
         }
@@ -135,18 +160,98 @@ static void tcp_server_task(void *pvParameters)
     vTaskDelete(NULL);
 }
 
-void app_main()
+
+// *********************************
+/* The examples use WiFi configuration that you can set via 'make menuconfig'.
+
+   If you'd rather not, just change the below entries to strings with
+   the config you want - ie #define EXAMPLE_WIFI_SSID "mywifissid"
+*/
+
+#define EXAMPLE_ESP_MAXIMUM_RETRY  10
+
+/* FreeRTOS event group to signal when we are connected*/
+static EventGroupHandle_t s_wifi_event_group;
+
+/* The event group allows multiple bits for each event, but we only care about one event
+ * - are we connected to the AP with an IP? */
+const int WIFI_CONNECTED_BIT = BIT0;
+
+static const char *TAG = "wifi station";
+
+static int s_retry_num = 0;
+
+static void event_handler(void* arg, esp_event_base_t event_base,
+                                int32_t event_id, void* event_data)
 {
-    ESP_ERROR_CHECK(nvs_flash_init());
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+        esp_wifi_connect();
+    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+        if (s_retry_num < EXAMPLE_ESP_MAXIMUM_RETRY) {
+            esp_wifi_connect();
+            xEventGroupClearBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+            s_retry_num++;
+            ESP_LOGI(TAG, "retry to connect to the AP");
+        }
+        ESP_LOGI(TAG,"connect to the AP fail");
+    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+        ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+        ESP_LOGI(TAG, "got ip:%s",
+                 ip4addr_ntoa(&event->ip_info.ip));
+        s_retry_num = 0;
+        xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+    }
+}
+
+void wifi_init_sta()
+{
+    s_wifi_event_group = xEventGroupCreate();
+
     tcpip_adapter_init();
+
     ESP_ERROR_CHECK(esp_event_loop_create_default());
 
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
 
-    /* This helper function configures Wi-Fi or Ethernet, as selected in menuconfig.
-     * Read "Establishing Wi-Fi or Ethernet Connection" section in
-     * examples/protocols/README.md for more information about this function.
-     */
-    ESP_ERROR_CHECK(example_connect());
+    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL));
+
+    wifi_config_t wifi_config = {};
+    //      strcpy((char*)wifi_config.sta.ssid, "PSK_GENT_LiTL");
+    //      strcpy((char*)wifi_config.sta.password, "wh{nOcr>Z8DJNUrN");
+          strcpy((char*)wifi_config.sta.ssid, "laptopWard");
+          strcpy((char*)wifi_config.sta.password, "labo1234");
+
+
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
+    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config) );
+    ESP_ERROR_CHECK(esp_wifi_start() );
+
+    ESP_LOGI(TAG, "wifi_init_sta finished.");
+    ESP_LOGI(TAG, "connect to ap SSID:%s",
+             "PSK_GENT_LiTL");
+}
+
+void app_main()
+{
+    //Initialize NVS
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+    ESP_LOGI(TAG, "ESP_WIFI_MODE_STA");
+    wifi_init_sta();
+
 
     xTaskCreate(tcp_server_task, "tcp_server", 4096, NULL, 5, NULL);
+
+//    // IP address.
+//    tcpip_adapter_get_ip_info(TCPIP_ADAPTER_IF_STA, &ipInfo);
+//    sprintf(str, "%x", ipInfo.ip.addr);
+//    printf("My IP: " IPSTR "\n", IP2STR(&ipInfo.ip));
 }
+
